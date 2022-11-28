b59616c2410c98f270bbb6686cc609684bfbdf87
eadcock <42786653+eadcock@users.noreply.github.com>
2022-11-28 14:27:28 -0500

    more touches, plus better recognition demo

--- python/Recognition.py
+++ python/Recognition.py
@@ -31,74 +31,78 @@ try:
 except:
     print("No Camera Source Found!")
 
-blendKernel = np.array([[0, -1, 0],
-                            [-1, 5, -1],
-                            [0, -1, 0]])
+def processImage(img, save, display):
+    blendKernel = np.array([[0, -1, 0],
+                                [-1, 5, -1],
+                                [0, -1, 0]])
 
-blended = cv2.filter2D(src=img, ddepth=-1, kernel=blendKernel)
+    blended = cv2.filter2D(src=img, ddepth=-1, kernel=blendKernel)
 
-# black and white
-grey = cv2.cvtColor(cv2.cvtColor(blended, cv2.COLOR_BGR2GRAY), cv2.COLOR_GRAY2BGR)
-
-cv2.imshow("raw", img)
-
-# contour analysis
-mask = cv2.Canny(grey, 50, 150)
-
-cv2.imshow("mask", mask)
-
-# close the contours by dilating and eroding
-kernal = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(6,6))
-closed = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernal)
-cv2.imshow("dilated", closed)
-
-# reverse the colors
-inverse = closed.copy()
-
-h,w = inverse.shape
-for i in range(h):
-    for j in range(w):
-        inverse[i, j] = 255 - inverse[i, j]
-
-# flood fill the background
-h,w = closed.shape
-_, filled, _, _ = cv2.floodFill(closed, None, (0,0), 255)
-_, filled, _, _ = cv2.floodFill(filled, None, (0,h-1), 255)
-_, filled, _, _ = cv2.floodFill(filled, None, (w-1,0), 255)
-_, filled, _, _ = cv2.floodFill(filled, None, (w-1,h-1), 255)
-
-cv2.imshow("inverse", inverse)
-cv2.imshow("filled", filled)
-
-# combine the flooded image and the inverse image
-# result = cv2.bitwise_and(filled, inverse)
-
-#cv2.imshow("result", result)
-
-cv2.imshow("masked result", cv2.bitwise_and(img, img, mask=filled))
-
-colored = cv2.bitwise_and(blended, blended, mask=filled)
-
-cv2.imshow("colored", colored)
-
-# blur the edges
-# this will create a smoother fall-off for plant generation
-# like the outskirts of a forest
-blurred = cv2.blur(filled, (10,10));
-cblurred = cv2.blur(colored, (10,10));
-
-# resize the image to be 100x100
-resized = cv2.resize(blurred, (100, 100), interpolation = cv2.INTER_AREA)
-cresized = cv2.resize(cblurred, (100, 100), interpolation = cv2.INTER_AREA)
-
-cv2.imshow("blended", blended)
-
-cv2.imshow("blurred", blurred)
-cv2.imshow("cblurred", cblurred)
+    # black and white
+    grey = cv2.cvtColor(cv2.cvtColor(blended, cv2.COLOR_BGR2GRAY), cv2.COLOR_GRAY2BGR)
 
 
-cv2.imwrite("../Assets/Resources/plant_output.png", resized)
-cv2.imwrite("../Assets/Resources//creature_output.png", cresized)
+    # contour analysis
+    mask = cv2.Canny(grey, 50, 150)
+
+
+    # close the contours by dilating and eroding
+    kernal = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(6,6))
+    closed = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernal)
+
+    # reverse the colors
+    inverse = closed.copy()
+
+    h,w = inverse.shape
+    for i in range(h):
+        for j in range(w):
+            inverse[i, j] = 255 - inverse[i, j]
+
+    # flood fill the background
+    h,w = closed.shape
+    _, filled, _, _ = cv2.floodFill(closed, None, (0,0), 255)
+    _, filled, _, _ = cv2.floodFill(filled, None, (0,h-1), 255)
+    _, filled, _, _ = cv2.floodFill(filled, None, (w-1,0), 255)
+    _, filled, _, _ = cv2.floodFill(filled, None, (w-1,h-1), 255)
+
+    # combine the flooded image and the inverse image
+    # result = cv2.bitwise_and(filled, inverse)
+
+    #cv2.imshow("result", result)
+
+    colored = cv2.bitwise_and(blended, blended, mask=filled)
+
+    # blur the edges
+    # this will create a smoother fall-off for plant generation
+    # like the outskirts of a forest
+    blurred = cv2.blur(filled, (10,10));
+    cblurred = cv2.blur(colored, (10,10));
+
+    # resize the image to be 100x100
+    resized = cv2.resize(blurred, (100, 100), interpolation = cv2.INTER_AREA)
+    cresized = cv2.resize(cblurred, (100, 100), interpolation = cv2.INTER_AREA)
+
+    # cv2.imshow("blended", blended)
+
+     cv2.imshow("cblurred", cblurred)
+
+    if display:
+        cv2.imshow("raw", img)
+        cv2.imshow("mask", mask)
+        cv2.imshow("dilated", closed)
+        cv2.imshow("inverse", inverse)
+        cv2.imshow("filled", filled)
+
+        cv2.imshow("colored", colored)
+
+        cv2.imshow("masked result", cv2.bitwise_and(img, img, mask=filled))
+        cv2.imshow("blurred", blurred)
+
+    if save:
+        cv2.imwrite("../Assets/Resources/plant_output.png", resized)
+        cv2.imwrite("../Assets/Resources//creature_output.png", cresized)
+
+processImage(img, True, True)
 
 while capture.isOpened():
     
@@ -109,6 +113,10 @@ while capture.isOpened():
 
     #cv2.imshow("Image", mask)
     #cv2.imwrite("G:\\Users\\cirea\\Documents\\Blackboard Assignment\\GameAI\\Assets\\Resources\\output.png", mask)
+
+    #_, frame = capture.read()
+    #processImage(frame, False, False)
+
 
     # Close the camera if 'q' is pressed
     if cv2.waitKey(1) == ord('q'):
